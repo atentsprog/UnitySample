3618b79dd36152ec4413546bfdb6162b22f82bfa
hktrollgames <hktrollgames@gmail.com>
2021-05-03 05:42:32 +0900

    미사일에 적이 데미지 입도록 수정

---
+++ Assets/Sample1/ArrowDamage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowDamage : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.GetComponent<Animator>().Play("GetHit", 0, 0);
+    }
+}
