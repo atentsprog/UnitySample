ac0d3b1aaa20fff4e4f5b07d2f1bee96b2192dd2
dochi486 <93mskim@gmail.com>
2021-05-03 20:39:16 +0900

    코루틴으로 다이모션
    슬라임 죽음

--- Assets/Monster.cs
+++ Assets/Monster.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,8 +13,27 @@ public class Monster : MonoBehaviour
     {
         Debug.Log("충돌");
 
-        Animation.Play("GetHit", 0, 0);
-       
+        //Animation.Play("Die", 0, 0);
+        StartCoroutine(DieAndDestroy());
 
+        
+
+    }
+    public float interval = 0.5f;
+    private IEnumerator DieAndDestroy()
+    {
+        GetComponent<Collider>().enabled = false;
+        enabled = false; //자기 자신을 꺼버리겠다 (움직이는 걸 끄고 다이모션 하게 만드는 코드), 게임 오브젝트를 false처리하면 모든 스크립트 동작이 멈춤
+
+        Animation.Play("Die", 0, 0);
+
+        yield return new WaitForSeconds(interval);
+        Destroy(gameObject);
+    }
+
+    public float speed = 3f;
+    private void Update()
+    {
+        transform.Translate(0, 0, speed * Time.deltaTime);
     }
 }
