6c33598820823d21ba00e59145213678be11c6ab
dochi486 <93mskim@gmail.com>
2021-05-03 18:54:14 +0900

    슬라임충돌로그

---
+++ Assets/Monster.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Ãæµ¹");
+
+    }
+}
