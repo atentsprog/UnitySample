7c0a18c65601672790852c6fa7c5afa89a602401
chanseon park <hktrollgames@gmail.com>
2021-05-30 12:14:16 +0900

    미사일 원하는 각도로 원하는 숫자만큼 생성하는 샘플 추가

---
+++ Assets/Sample5/BossSpaceShip.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpaceShip : MonoBehaviour
+{
+    public MoveLocal missleGo;
+
+    public float totalAngle = 360;
+    public int count = 36;
+
+    public Vector3 missleSpawnAxis = new Vector3(0, 0, 1);
+    public Vector3 missleMoveAxis = new Vector3(0, 1, 0);
+    public void SpawnMissileFn()
+    {
+        float angle = 0;
+        if(count > 1)
+            angle = totalAngle / (count - 1);
+        float startAngle = totalAngle * -0.5f;// (count * 0.5f - count);
+        for (int i = 0; i < count; i++)
+        {
+            //var toAngle = transform.forward; // ( 0, 0, 1)
+            var eulerAngle = transform.eulerAngles; // (0, 0, 180)
+            if(count > 1)
+                eulerAngle += missleSpawnAxis * (startAngle + (angle * i));
+
+            Quaternion lookRotation = Quaternion.Euler(eulerAngle);
+            MoveLocal moveLocal = Instantiate(missleGo, transform.position, lookRotation);
+
+            moveLocal.moveAxis = missleMoveAxis;
+        }
+    }
+}
---
+++ Assets/Sample5/MoveLocal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLocal : MonoBehaviour
+{
+    public Vector3 moveAxis = new Vector3(0, 1, 0);
+    public float speed = 1;
+    void Update()
+    {
+        transform.Translate(moveAxis * speed * Time.deltaTime, Space.Self);
+    }
+}
