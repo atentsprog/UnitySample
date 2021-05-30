35c185447f08cf356f2227cb6b636c3db7e71331
shinepcs <shinepcs@gmail.com>
2021-05-30 10:21:57 +0900

    리소스 폴더 로드 샘플 추가

---
+++ Assets/Sample5/ResourceLoadTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceLoadTest : MonoBehaviour
+{
+    public string readObjectName;
+    public void LoadResourceFn()
+    {
+        // 메모리상으로 로드, 게임오브젝트, 이미지, 사운드,.. 모든것 로드 가능.
+        // 파일 이름 같을 경우 타입지정해서 구분 가능
+        Object memoryObj = Resources.Load(readObjectName);
+        //Object obj = Resources.Load(readObjectName, typeof(GameObject));
+
+        GameObject memoryGo = (GameObject)memoryObj;
+        GameObject sceneGo = Instantiate(memoryGo);
+        sceneGo.name = "리소스에서 불러온" + memoryGo.name;
+    }
+}
