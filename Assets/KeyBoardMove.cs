7febdd9cfb9d6334a65ea9168c1954749f211371
hktrollgames <hktrollgames@gmail.com>
2021-06-30 07:43:44 +0900

    공격감지 방식 샘플 작업중

---
+++ Assets/KeyBoardMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyBoardMove : MonoBehaviour
+{
+    public float speed = 5;
+    // Update is called once per frame
+    void Update()
+    {
+        // WASD, W위로, A왼쪽,S아래, D오른쪽
+        Vector3 move = new Vector3(0, 0, 0); // Vector3.zero
+
+        // || -> or
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) move.z = 1;
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) move.z = -1;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) move.x = -1;
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) move.x = 1;
+
+        Vector3 position = transform.position;
+        position.x = position.x + move.x * speed * Time.deltaTime;
+        position.z = position.z + move.z * speed * Time.deltaTime;
+
+        move.Normalize();
+        transform.position = position;
+    }
+}
---
+++ Assets/PlayerCollider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Smaple8.Collider
+{
+    public class PlayerCollider : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Keypad1))
+            {
+                Attack();
+            }
+        }
+
+        public GameObject attackCollider;
+        private void Attack()
+        {
+            attackCollider.SetActive(true);
+            attackCollider.SetActive(false);
+        }
+    }
+}
