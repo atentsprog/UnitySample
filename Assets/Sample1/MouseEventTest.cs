021683125ce01d55f43e766087a6d30bd281f4ec
hktrollgames <hktrollgames@gmail.com>
2021-06-29 10:10:33 +0900

    마우스 이벤트 적용, 카메라 Pan이동, 터치시 플레이어 네비메시 이동

---
+++ Assets/Sample1/MouseEventTest.cs
@@ -0,0 +1,13 @@
+﻿//using DigitalRubyShared;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.EventSystems;
+
+public class MouseEventTest : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Debug.Log("OnMouseDown");
+    }
+}
---
+++ Assets/Sample1/NavMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMove : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public LayerMask layer;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hitData, 1000, layer))
+            {
+                agent.destination = hitData.point;
+            }
+        }
+    }
+}
