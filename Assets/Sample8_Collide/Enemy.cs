f777434377ff879097980b7a52716bbaca556715
hktrollgames <hktrollgames@gmail.com>
2021-06-30 10:09:42 +0900

    충돌 감지 샘플 추가

---
+++ Assets/Sample8_Collide/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Smaple8.Collider
+{
+    public class Enemy : MonoBehaviour
+    {
+        public float vibrate = 0.01f;
+        public static List<Enemy> Items = new List<Enemy>();
+        private void Awake()
+        {
+            Items.Add(this);
+        }
+
+        void Update()
+        {
+            if(vibrate > 0)
+            {
+                transform.Translate(Random.Range(-vibrate, vibrate), 0, 0);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            Items.Remove(this);
+        }
+    }
+}
---
+++ Assets/Sample8_Collide/MeleeAttackCollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Smaple8.Collider
+{
+    public class MeleeAttackCollider : MonoBehaviour
+    {
+        // RigidBody 움직이는거랑 상관없이 감지됨.
+        private void OnTriggerEnter(UnityEngine.Collider other)
+        {
+            print("밀리어택 콜라이더에 부딪힘,Trigger " +  other);
+        }
+
+        /// <summary>
+        /// RigidBody가 있는 쪽이 움직이고 있어야 감지됨.
+        /// 움직이지 않고 있다면 랜덤하게 감지됨 <- 사용못함
+        /// </summary>
+        /// <param name="collision"></param>
+        private void OnCollisionEnter(Collision collision)
+        {
+            print("밀리어택 콜라이더에 부딪힘, Collision " + collision);
+        }
+    }
+}
