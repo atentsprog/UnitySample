9f41ca3ac09b9af77842a227966f800fdf1be43d
hktrollgames <hktrollgames@gmail.com>
2021-07-14 06:24:33 +0900

    BgmPlayer 샘플 추가

---
+++ Assets/BgmPlayer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BgmPlayer : MonoBehaviour
+{
+    [System.Serializable]
+    public class BgmInfo
+    {
+        public AudioClip audioClip;
+        public float volume;
+
+        public float fadeInTime = 3;
+        public float fadeInBeginVolume = 0.3f;
+
+        public float fadeOutTime = 2;
+    }
+
+    public BgmInfo[] Bgms;
+    int playBgmIndex;
+
+
+    AudioSource[] audioSources;
+    int audioSourcesIndex;
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        audioSources = GetComponents<AudioSource>();
+        foreach (var item in audioSources)
+        {
+            item.loop = true;
+            item.spatialBlend = 0;
+        }
+
+        PlayBgmSound(Bgms[playBgmIndex]);
+    }
+
+    private void PlayBgmSound(BgmInfo bgm)
+    {
+        AudioSource audioSource = audioSources[audioSourcesIndex];
+        audioSource.clip = bgm.audioClip;
+        audioSource.volume = bgm.volume;
+
+        if (bgm.fadeInTime > 0)
+        {
+            audioSource.volume = bgm.fadeInBeginVolume;
+            DOTween.To(() => audioSource.volume, x => audioSource.volume = x, bgm.volume, bgm.fadeInTime);
+        }
+        else
+        {
+            audioSource.volume = bgm.volume;
+        }
+
+        audioSource.Play();
+    }
+
+    public void PlayNextBgm()
+    {
+        StopBgmSound(Bgms[playBgmIndex]);
+
+        playBgmIndex++;
+        if (playBgmIndex >= Bgms.Length)
+            playBgmIndex = 0;
+
+
+        audioSourcesIndex++;
+        if (audioSourcesIndex >= audioSources.Length)
+            audioSourcesIndex = 0;
+
+
+        PlayBgmSound(Bgms[playBgmIndex]);
+    }
+
+    private void StopBgmSound(BgmInfo bgmInfo)
+    {
+        var audioSource = audioSources[audioSourcesIndex];
+        if (bgmInfo.fadeOutTime > 0)
+        {
+            DOTween.To(() => audioSource.volume, x => audioSource.volume = x, 0, bgmInfo.fadeOutTime)
+                .OnComplete(()=> {
+                    audioSource.Stop();
+                    audioSource.clip = null;
+                });
+        }
+        else
+        { 
+            audioSource.Stop();
+            audioSource.clip = null;
+        }
+    }
+}
---
+++ Assets/Sample9_PlaySound/BgmSound.cs
@@ -0,0 +1,47 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgmSound : MonoBehaviour
+{
+    public float volume = 1;
+    public AudioClip[] audioClip = new AudioClip[3];
+
+    AudioSource audioSource;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = volume;
+        audioSource.loop = true;
+        StartCoroutine(PlayBgm());
+    }
+
+    float soundEndTime;
+    
+    private IEnumerator PlayBgm()
+    {
+        while(true)
+        {
+            for (int i = 0; i < audioClip.Length; i++)
+            {
+                audioSource.clip = audioClip[i];
+                audioSource.Play();
+                float soundtime = audioClip[i].length;
+                soundEndTime = Time.time + soundtime;
+                while (soundEndTime > Time.time)
+                    yield return null;
+            }
+            yield return null; 
+        }
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            // 다음 노래 재생하기.
+            soundEndTime = 0;
+        }
+    }
+}
