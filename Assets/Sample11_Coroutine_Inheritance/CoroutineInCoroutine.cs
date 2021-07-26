e4a7d667bbfcf29bc942f8cdd2ba107060886f73
hktrollgames <hktrollgames@gmail.com>
2021-07-26 14:46:04 +0900

    중첩 코루틴과 상속리팩토링 샘플

---
+++ Assets/Sample11_Coroutine_Inheritance/CoroutineInCoroutine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sample11_Coroutine_Inheritance
+{
+    public class CoroutineInCoroutine : MonoBehaviour
+    {
+        /// <summary>
+        /// Start, OnTriggerEnter, OnCollisionEnter 3개 함수는 반환형이 IEnumerator일경우 코루틴처럼 작동
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator Start()
+        {
+            yield return StartCoroutine(Co1());
+            StartCoroutine(Co2("StartCoroutine으로 호출 1"));
+            StartCoroutine(Co1());
+            Co2("StartCoroutine 없이 Coroutine 함수 호출");
+            StartCoroutine(Co2("StartCoroutine으로 호출 2"));
+        }
+
+        IEnumerator Co1()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                yield return Co2(i.ToString());
+            }
+        }
+
+        private IEnumerator Co2(string log)
+        {
+            Debug.Log(log);
+            yield return null;
+            Debug.Log(log + "End");
+        }
+    }
+}
---
+++ Assets/Sample11_Coroutine_Inheritance/Inheritance.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sample11_Coroutine_Inheritance
+{
+    class Parent
+    {
+
+    }
+    class Child1 : Parent
+    {
+        public int childInt;
+        public void Child1Fn()
+        {
+            Debug.Log("Child1Fn");
+        }
+    }
+    class Child2
+    {
+        public void Child2Fn()
+        {
+            Debug.Log("Child2Fn");
+        }
+    }
+
+    public class Inheritance : MonoBehaviour
+    {
+        void Start()
+        {
+            //Child1 child1 = new Child1();
+            //child1.Child1Fn();
+        }
+    }
+}
