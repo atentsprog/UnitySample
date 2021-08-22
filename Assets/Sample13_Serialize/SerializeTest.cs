8f25857023256313683f29c1b864e05aa988802a
hktrollgames <hktrollgames@gmail.com>
2021-08-22 23:35:46 +0900

    딕셔너리 시리얼라이즈 샘플 추가

---
+++ Assets/Sample13_Serialize/SerializeTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SerializeTest : MonoBehaviour
+{
+
+    public SerializableDictionary<string, string> strStrDic = new SerializableDictionary<string, string>();
+    public SerializableDictionary<string, int> inventoryData = new SerializableDictionary<string, int>();
+    public SerializableDictionary<int, int> intIntDic = new SerializableDictionary<int, int>();
+
+    string inventoryDataKey = "inventoryData";
+    void Start()
+    {
+        strStrDic["string"] = "test";
+        inventoryData["str1"] = 1;
+        intIntDic[1] = 2;
+    }
+
+    [ContextMenu("Save")]
+    private void SaveInvenData()
+    {
+        string json = JsonUtility.ToJson(inventoryData);
+        try
+        {
+            PlayerPrefs.SetString(inventoryDataKey, json);
+        }
+        catch (System.Exception err)
+        {
+            Debug.Log($"Error:{err}");
+        }
+    }
+
+    [ContextMenu("Load")]
+    private void LoadInvenData()
+    {
+        inventoryData = JsonUtility.FromJson<SerializableDictionary<string, int>>(PlayerPrefs.GetString(inventoryDataKey));
+        if (inventoryData == null)
+        {
+            Debug.LogWarning("record == null");
+            return;
+        }
+    }
+}
