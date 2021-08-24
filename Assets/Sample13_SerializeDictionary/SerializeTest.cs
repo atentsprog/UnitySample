5f6e6fcf2f64477441b8320bac417e4e666e428c
hktrollgames <hktrollgames@gmail.com>
2021-08-24 12:20:33 +0900

    List<int> 시리얼라이즈 샘플 추가

--- Assets/Sample13_SerializeDictionary/SerializeTest.cs
+++ Assets/Sample13_SerializeDictionary/SerializeTest.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//출처 http://wehappyfamily.blogspot.com/2017/12/unity-local-db-c-jsonutility.html
+[System.Serializable]
+public class SerializationList<T>
+{
+    [SerializeField]
+    List<T> target;
+    public List<T> ToList() { return target; }
+
+    public SerializationList(List<T> target)
+    {
+        this.target = target;
+    }
+}
+
 public class SerializeTest : MonoBehaviour
 {
-
     public SerializableDictionary<string, string> strStrDic = new SerializableDictionary<string, string>();
     public SerializableDictionary<string, int> inventoryData = new SerializableDictionary<string, int>();
     public SerializableDictionary<int, int> intIntDic = new SerializableDictionary<int, int>();
+    public List<int> intList = new List<int>();
 
     string inventoryDataKey = "inventoryData";
+    string intListKey = "intList";
     void Start()
     {
         strStrDic["string"] = "test";
@@ -20,6 +35,13 @@ public class SerializeTest : MonoBehaviour
     [ContextMenu("Save")]
     private void SaveInvenData()
     {
+        string intJson = JsonUtility.ToJson(intList);
+        string intSerializationJson = JsonUtility.ToJson(new SerializationList<int>(intList));
+        print(intJson + "" + intJson.Length);
+        print(intSerializationJson + "" + intSerializationJson.Length);
+        PlayerPrefs.SetString(intListKey, intSerializationJson);
+
+
         string json = JsonUtility.ToJson(inventoryData);
         try
         {
@@ -34,6 +56,9 @@ public class SerializeTest : MonoBehaviour
     [ContextMenu("Load")]
     private void LoadInvenData()
     {
+        intList = JsonUtility.FromJson<SerializationList<int>>(PlayerPrefs.GetString(intListKey)).ToList();
+
+
         inventoryData = JsonUtility.FromJson<SerializableDictionary<string, int>>(PlayerPrefs.GetString(inventoryDataKey));
         if (inventoryData == null)
         {
