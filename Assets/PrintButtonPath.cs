8606ff067b2218795fc99a7450b035f0185e28b5
hktrollgames <hktrollgames@gmail.com>
2021-10-21 14:23:48 +0900

    3D UI샘플 추가

---
+++ Assets/PrintButtonPath.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+namespace Sample20_3D_UI
+{
+    public class PrintButtonPath : MonoBehaviour
+    {
+
+        void Start()
+        {
+            var buttons = GetComponentsInChildren<Button>();
+            foreach (var item in buttons)
+            {
+                item.onClick.AddListener(() =>
+                {
+                    print(item.transform.GetPath());
+                    item.transform.DOPunchScale(Vector3.one * 0.9f, 0.4f);
+                });
+            }
+
+            //GetComponentsInChildren<Button>().ForEach(item =>
+            //{
+            //    item.onClick.AddListener(() =>
+            //    {
+            //        print(item.transform.GetPath());
+            //        item.transform.DOPunchScale(Vector3.one * 0.9f, 0.4f);
+            //    });
+            //});
+        }
+    }
+
+    //static public class Ext
+    //{
+    //    public static void ForEach<T>(this IEnumerable<T> list, System.Action<T> action)
+    //    {
+    //        foreach (var item in list)
+    //            action(item);
+    //    }
+    //}
+}
