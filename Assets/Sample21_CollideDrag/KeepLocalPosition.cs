d441b2cc25a00bd90e317d1eef6e65fc0e92e22b
hktrollgames <hktrollgames@gmail.com>
2021-11-10 17:21:36 +0900

    물리 드래그 예시 샘플 추가

---
+++ Assets/Sample21_CollideDrag/KeepLocalPosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sample21_CollideDrag
+{
+    public class KeepLocalPosition : MonoBehaviour
+    {
+        Vector3 originalLocalPos;
+        public float lerpValue = 0.05f;
+
+        void Start()
+        {
+            originalLocalPos = transform.localPosition;
+        }
+
+        void Update()
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, originalLocalPos, lerpValue);
+        }
+    }
+}
---
+++ Assets/Sample21_CollideDrag/RemoveRigidbodyVelocity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sample21_CollideDrag
+{
+    public class RemoveRigidbodyVelocity : MonoBehaviour
+    {
+        new Rigidbody rigidbody;
+        void Start()
+        {
+            rigidbody = GetComponent<Rigidbody>();
+
+        }
+        void Update()
+        {
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.angularVelocity = Vector3.zero;
+        }
+    }
+}
